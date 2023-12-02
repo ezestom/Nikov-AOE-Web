41820201140be59315d0368ce38ef5ff6fc7d504
eze stom <ezequielstom@gmail.com>
2023-12-01 21:38:17 -0300

    Update imports and fix layout in Section component

--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -10,7 +10,7 @@ export function Youtube() {
 				style={{ borderRadius: "8px", height: "100%", width: "100%" }}
 				src="https://www.youtube.com/embed/jF2R-DS6ApI?si=1x1CkZ4cci5ibE93&amp;controls=0&amp;start=10&mute=1&autoplay=1"
 				title="YouTube video player"
-				frameborder="0"
+				// frameborder="0"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 				allowfullscreen></iframe>
 		</article>
