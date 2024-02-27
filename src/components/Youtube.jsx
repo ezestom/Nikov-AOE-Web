2df32c7c0f7d4aa75c478e16dc17d05b4b6e1c8f
eze stom <ezequielstom@gmail.com>
2024-02-27 10:54:47 -0300

    Update logo references

--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -4,11 +4,11 @@ export function Youtube() {
 	return (
 		<article class="youtube relative flex flex-col items-center h-full w-full">
 			<span class="absolute text-sm font-medium text-yellow-500">
-				Nicov Best Highlights
+				Sebastian 2002 AOE
 			</span>
 			<iframe
 				style={{ borderRadius: "8px", height: "100%", width: "100%" }}
-				src="https://www.youtube.com/embed/jF2R-DS6ApI?si=1x1CkZ4cci5ibE93&amp;controls=0&amp;start=10&mute=1&autoplay=1"
+				src="https://www.youtube.com/embed/DsaUGDua8n8?autoplay=1&mute=1"
 				title="YouTube video player"
 				// frameborder="0"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
